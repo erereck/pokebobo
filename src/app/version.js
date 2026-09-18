48ae2ff76237d54709a167eb86529dcef432a6bf
erereck <131626550+erereck@users.noreply.github.com>
2026-09-18 11:35:02 -0300

    chore(release): name 0.5.0 Legado

--- src/app/version.js
+++ src/app/version.js
@@ -1,3 +1,3 @@
 import packageInfo from "../../package.json" with { type: "json" };
 export const VERSION = packageInfo.version;
-export const RELEASE_NAME = "Semanas Vivas";
+export const RELEASE_NAME = "Legado";
