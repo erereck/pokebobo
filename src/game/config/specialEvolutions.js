cb9aba32dec5b1a2cc251970e37f4f8ce539f334
erereck <131626550+erereck@users.noreply.github.com>
2026-09-18 11:29:35 -0300

    feat(evolution): define level-only special evolution rules

---
+++ src/game/config/specialEvolutions.js
@@ -0,0 +1,10 @@
+export const SPECIAL_EVOLUTION_LEVELS = Object.freeze({
+  levelFriendship: 20,
+  levelMove: 28,
+  levelExtra: 28,
+  levelHold: 30,
+  useItem: 32,
+  trade: 32,
+});
+
+export const DEFAULT_SPECIAL_EVOLUTION_LEVEL = 30;
